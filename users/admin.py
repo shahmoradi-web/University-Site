16a376ff6c9cf22b6bec6f12d46ed4f53cbe71a9
narges <narges.shahmoradi.01@gmail.com>
2025-10-14 21:02:22 +0330

    add StudentProfile and  TeacherProfile Admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'student_id',
+        'department',
+        'entry_term'
+    ]
+    search_fields = [
+        'student_id',
+        'major',
+        'faculty'
+
+    ]
+    list_filter = [
+        'faculty',
+        'entry_term',
+        'department',
+        'grade',
+    ]
+    ordering = ('student_id',)
+
+@admin.register(TeacherProfile)
+class TeacherProfileAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'department',
+        'teacher_code'
+    ]
+    search_fields = [
+        'user__username',
+        'department',
+        'teacher_code'
+    ]
+    list_filter = [
+        'department',
+        'teacher_code',
+    ]
+    ordering = ('teacher_code',)
