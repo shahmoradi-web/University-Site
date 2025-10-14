1d50a605dac0517e049905c6e22855a582ef6270
narges <narges.shahmoradi.01@gmail.com>
2025-10-14 20:50:38 +0330

    add model StudentProfile and TeacherProfile

--- users/models.py
+++ users/models.py
@@ -1,4 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
+class StudentProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    student_id = models.IntegerField(unique=True)
+    father_name = models.CharField(max_length=100)
+    faculty = models.CharField(max_length=100)
+    major = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    entry_term = models.CharField(max_length=100)
+    grade = models.IntegerField()
+
+
+class TeacherProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    teacher_code = models.IntegerField(unique=True)
+    department = models.CharField(max_length=100)
