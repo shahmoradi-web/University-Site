a41dc6450345f00e659282fc0042aede820b075d
narges <narges.shahmoradi.01@gmail.com>
2025-10-14 20:42:05 +0330

    add users app

--- UniversitySite/settings.py
+++ UniversitySite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
@@ -122,7 +123,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-AUTH_USER_MODEL = 'account.StudentUser'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
