e489ea9ab71f5fd0e9fcabefc52168157e7a69ed
narges <narges.shahmoradi.01@gmail.com>
2025-10-16 21:41:55 +0330

    add course and enrollment admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = [
+        'name', 'teacher','credit'
+    ]
+    list_filter = ['teacher', 'credit']
+    search_fields = ['name']
+    ordering = ['name']
+
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ['course','user','teacher']
+    list_filter = ['course','user','teacher', 'enrolled_at','status']
+    search_fields = ['enrolled_at','status', 'enrolled_at']
+    ordering = ['enrolled_at']
