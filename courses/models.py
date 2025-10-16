34a4a2984b4a8365dedcbcb10002d19b7d7298a7
narges <narges.shahmoradi.01@gmail.com>
2025-10-16 21:33:40 +0330

    add model courses

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,17 @@
+
+from UniversitySite.settings import AUTH_USER_MODEL as User
 from django.db import models
 
 # Create your models here.
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE)
+    term = models.CharField(max_length=100)
+    credit = models.IntegerField(default=3)
+
+    def __str__(self):
+        return self.name
+
+
