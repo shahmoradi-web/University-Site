250b368c28260b38f7b12c3892a0580596782e28
narges <narges.shahmoradi.01@gmail.com>
2025-10-30 14:16:15 +0330

    add Email Feature

--- UniversitySite/settings.py
+++ UniversitySite/settings.py
@@ -137,3 +137,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email config
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'unversitysite.app@gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = '<PASSWORD>'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
